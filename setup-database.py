c7e7c3e81011ed610cd55bb1bf12ab1a52add487
maria <maria.siitonen@metropolia.fi>
2025-12-02 14:08:43 +0200

    main page

--- setup-database.py
+++ setup-database.py
@@ -12,7 +12,7 @@ def setup_database():
     # Connection details - UPDATE THESE
     DB_HOST = 'localhost'
     DB_USER = 'root'
-    DB_PASSWORD = 'Cde3xsw2'  # <-- UPDATE WITH YOUR PASSWORD
+    DB_PASSWORD = 'metropolia'  # <-- UPDATE WITH YOUR PASSWORD
     DB_NAME = 'creature_catcher'
 
     try:
