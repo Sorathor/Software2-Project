8bf7a6cb29a8df337a02f80da4d00cc217fd0f2a
fozia mumin <fauzahmumin@gmail.com>
2025-12-02 13:21:43 +0200

    login.c

---
+++ loginpage/login.js
@@ -0,0 +1,14 @@
+const btn =document.getElementById("enterBtn")
+const username = document.getElementById("username")
+
+
+btn.addEventListener("click",() => {
+      if(username.value.trim()=== ""){
+      alert("please enter a username to continue,");
+      }else{
+
+
+      }
+
+
+});
